bfca98dafc712fd2979147831d87ab265e065fd1
tristan <tristanbmuri@gmail.com>
2025-10-02 17:52:09 +0200

    innit

---
+++ backend/workflow/default_workflow.py
@@ -0,0 +1,31 @@
+import os
+
+from backend.main.config import create_config
+
+from backend.main.model_builder import create_llm
+from backend.main.prompt_builder import build_langfuse_prompt, get_messages
+
+
+def default_workflow(data, **kwargs):
+    if 'config' in kwargs:
+        config = kwargs['config']
+    else:
+        config = create_config("google")
+
+    llm = create_llm(config)
+
+    if 'prompt_name' in kwargs:
+        prompt_name = kwargs['prompt']
+    else:
+        prompt_name = "default"
+    if 'tag' in kwargs:
+        tag = kwargs['tag']
+    else:
+        tag = "production"
+
+    prompt = build_langfuse_prompt(prompt_name, llm, tag)
+
+    messages = get_messages(data, **kwargs)
+
+    re = llm.invoke(messages)
+    return re
