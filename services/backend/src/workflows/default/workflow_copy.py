741bb02e6fe56e04fef727ae6849d97965de0008
tristan <tristanbmuri@gmail.com>
2025-10-31 17:02:46 +0100

    prep before rebase

---
+++ services/backend/src/workflows/default/workflow_copy.py
@@ -0,0 +1,23 @@
+from langfuse._client.observe import observe
+
+from ..helpers.config import create_config
+from ..helpers.model_builder import create_llm
+from ..helpers.prompt_builder import get_messages
+
+
+@observe(name="default-workflow")
+def default_workflow(user_input, **kwargs):
+    if 'config' in kwargs:
+        config = kwargs['config']
+    else:
+        config = create_config("google")
+
+    llm = create_llm(config)
+
+    from langchain_core.prompts import ChatPromptTemplate
+    prompt = ChatPromptTemplate.from_template("{input}")
+
+    messages = get_messages(prompt, input=user_input, **kwargs)
+
+    re = llm.invoke(messages)
+    return re.content
